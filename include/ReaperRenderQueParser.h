2ada756e33392b7965e351f7c38e3250e46446ac
Simon Gumbleton <Simongumbleton@gmail.com>
2018-08-02 14:10:07 +0100

    Adding struct for render job

--- include/ReaperRenderQueParser.h
+++ include/ReaperRenderQueParser.h
@@ -1,5 +1,16 @@
 #pragma once
+#include "WwiseConnectionHandler.h"
 
+
+struct RenderQueJob
+{
+	std::string RenderQueFilePath;
+	WwiseObject parentWwiseObject;
+	std::vector<std::string> RenderQueJobFileList;
+	std::string ParentReaperProject;
+};
+
+std::vector<RenderQueJob> ListOfRenderQueJobs;
 
 void ParseRenderQueFile(std::string pathToQueFile);
 
